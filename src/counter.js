497c184da2f9424062325ff8adaf5132de0801a0
Muhammet Yasin ARLI <myasinarli@gmail.com>
2020-04-30 09:25:58 +0300

    added eslintrc for static code analysis

--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Proptypes from 'prop-types'
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import './styles/main.css';
+import React from "react";
+import Proptypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "./styles/main.css";
 
 export class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             counterVal: 0
-        }
+        };
         this.increase = this.increase.bind(this);
         this.decrease = this.decrease.bind(this);
         this.reset = this.reset.bind(this);
@@ -35,17 +35,17 @@ export class Counter extends React.Component {
         return <div>
             <h1>{this.props.counterHeader} {this.state.counterVal}</h1>
             <ButtonGroup>
-                <Button variant="success" className="fxWidth" onClick={this.increase}>Increase</Button>
-                <Button variant="warning" className="fxWidth"  onClick={this.decrease}>Decrease</Button>
-                <Button variant="danger" className="fxWidth"  onClick={this.reset}>Reset</Button>
+                <Button variant="primary" className="fxWidth" onClick={this.increase}>Increase</Button>
+                <Button variant="primary" className="fxWidth"  onClick={this.decrease}>Decrease</Button>
+                <Button variant="primary" className="fxWidth"  onClick={this.reset}>Reset</Button>
             </ButtonGroup>
-        </div>
+        </div>;
     }
 }
 Counter.propTypes = {
     counterHeader: Proptypes.string.isRequired
 
-}
+};
 Counter.defaultProps = {
     counterHeader: "Counter Value : "
-}
+};
